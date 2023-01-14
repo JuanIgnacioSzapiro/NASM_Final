5ab1e924e21449aff1da8065dac9c51430f3f12a
Juan Igmacio Szapiro <juanignacioszapiro@gmail.com>
2023-01-14 14:55:55 -0300

    update 3

---
+++ 3/3.asm
@@ -0,0 +1,94 @@
+;Se ingresa un año. La computadora indica si es, o no, bisiesto.
+
+; "D:\NASM\nasm.exe" -f win32 3.asm --PREFIX _
+
+; gcc.exe 3.obj -o 3.exe
+
+; 3.exe
+
+global _start
+global main
+extern printf
+extern scanf
+extern exit
+extern gets
+
+section .bss
+    numero:
+        resd 1 ; 1 dword (4 bytes)
+    cadena:
+        resb 0x0100 ; 256 bytes
+
+section .data
+    fmtInt:
+        db "%d", 0 ; FORMATO PARA NUMEROS ENTEROS
+    fmtString:
+        db "%s", 0 ; FORMATO PARA CADENAS
+    esBisiesto:
+        db "Es un año bisiesto", 0 
+    noEsBisiesto:
+        db "No es un año bisiesto", 0 
+
+section .text
+    mostrarCadena: ; RUTINA PARA MOSTRAR UNA CADENA USANDO PRINTF
+        push cadena 
+        push fmtString 
+        call printf 
+        add esp, 8 
+        ret
+    leerNumero: ; RUTINA PARA LEER UN NUMERO ENTERO USANDO SCANF
+        push numero
+        push fmtInt
+        call scanf
+        add esp, 8
+        ret
+    salirDelPrograma: ; PUNTO DE SALIDA DEL PROGRAMA USANDO EXIT
+        push 0
+        call exit
+    mostrarRsultadoNegativo:
+        push noEsBisiesto
+        push fmtString 
+        call printf 
+        add esp, 8 
+        ret
+    mostrarRsultadoPositivo:
+        push esBisiesto
+        push fmtString 
+        call printf 
+        add esp, 8 
+        ret
+main:
+start:
+    call leerNumero
+
+    mov eax, [numero]
+    mov ecx, 4
+    mov edx, 0
+    div ecx
+    cmp edx, 0
+    jne salirMal
+
+    mov eax, [numero]
+    mov ecx, 100
+    mov edx, 0
+    div ecx
+    cmp edx, 0
+    je salirMal
+
+    salirBien:
+        call  mostrarRsultadoPositivo
+        call salirDelPrograma
+
+    salirMal:
+        mov eax, [numero]
+        mov ecx, 400
+        mov edx, 0
+        div ecx
+        cmp edx, 0
+        je salirBien
+        call  mostrarRsultadoNegativo
+        call salirDelPrograma
+
+    
+
+
