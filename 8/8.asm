3893667dbceccc6d267c0b1079a46bfae3e82786
Juan Igmacio Szapiro <juanignacioszapiro@gmail.com>
2023-01-22 11:49:36 -0300

    update 8
    final

--- 8/8.asm
+++ 8/8.asm
@@ -3,6 +3,16 @@
 ; "D:\NASM\nasm.exe" -f win32 8.asm --PREFIX _
 ; gcc.exe 8.obj -o 8.exe
 ; 8.exe
+; 3
+; 1
+; 2
+; 3
+; 4
+; 5
+; 6
+; 7
+; 8
+; 9
 
 ; 1 2 3   1 4 7
 ; 4 5 6   2 5 8
@@ -71,6 +81,12 @@ section .text
         call printf
         add esp, 4 
         ret
+    mostrarPedirCantFilasYColumnas:
+        push pedirCantFilasYColumnas 
+        push fmtString 
+        call printf 
+        add esp, 8 
+        ret
     mostrarPedirValorIngresadoP1: ; RUTINA PARA MOSTRAR UNA CADENA USANDO PRINTF
         push pedirValorIngresadoP1 
         push fmtString 
@@ -107,8 +123,127 @@ section .text
         call printf 
         add esp, 8 
         ret
+    mostrarParteMatriz: ; para testeo en 'empieza'
+        mov eax, [contadorColumnas] ; posicion = contadorColumnas * cantidadFilasYColumnas
+        mov ecx, [cantidadFilasYColumnas]
+        mov edx, 0
+        mul ecx
+        mov [posicion], eax
 
+        mov eax, [contadorFilas] ; posicion += contadorFilas
+        add [posicion], eax
+
+        mov esi, [posicion]
+        mov al, byte dword[esi + matriz]
+
+        mov [valorIngresado], al
+
+        push dword[valorIngresado] ; NO HACER: push dword[esi + matriz]
+        push fmtInt 
+        call printf 
+        add esp, 8 
+        ret
+    salirDelPrograma: ; PUNTO DE SALIDA DEL PROGRAMA USANDO EXIT
+        push 0
+        call exit
+    inicializarFilas:
+        mov ecx, 0
+        mov [contadorFilas], ecx
+        ret
+    inicializarColumnas:
+        mov ecx, 0
+        mov [contadorColumnas], ecx
+        ret
 main:
 _start:
-    call pedirCantFilasYColumnas
-    call leerCantFilas
+    call inicializarFilas
+    call inicializarColumnas
+
+    corroborarFilasYColumnas:
+        call mostrarPedirCantFilasYColumnas
+        call leerCantidadFilasYColumnas
+        
+        mov eax, 1 ; if ( cantidadFilasYColumnas < 1 )
+        cmp [cantidadFilasYColumnas], eax
+        jle corroborarFilasYColumnas
+
+        mov eax, 10 ; if ( cantidadFilasYColumnas > 10 )
+        cmp [cantidadFilasYColumnas], eax
+        jg corroborarFilasYColumnas
+
+    empieza:
+        mov ecx, [contadorFilas] ; if ( contadorFilas > cantidadFilasYColumnas )
+        mov eax, [cantidadFilasYColumnas]
+        sub eax, 1
+        cmp ecx, eax
+        jg mostrarMatriz
+        
+        call mostrarPedirValorIngresadoP1
+        call mostrarContadorFilas
+        call mostrarPedirValorIngresadoP2
+        call mostrarContadorColumnas
+        call mostrarIgualEspacio
+        call leerValorIngresado
+
+        mov eax, [contadorFilas] ; posicion = contadorFilas * cantidadFilasYColumnas
+        mov ecx, [cantidadFilasYColumnas]
+        mov edx, 0
+        mul ecx
+        mov [posicion], eax
+
+        mov eax, [contadorColumnas] ; posicion += contadorColumnas
+        add [posicion], eax
+
+        mov esi, [posicion]
+        mov al, byte dword[valorIngresado]
+        mov [esi + matriz], al
+
+        mov ecx, [contadorColumnas] ; contadorColumnas++
+        add ecx, 1
+        mov [contadorColumnas], ecx
+
+        mov eax, [cantidadFilasYColumnas]
+        sub eax, 1
+        cmp ecx, eax ; if ( contadorColumnas <= cantidadFilasYColumnas )
+        jle empieza
+
+        call inicializarColumnas ; contadorColumnas = 0
+
+        mov ecx, [contadorFilas] ; contadorColumnas++
+        add ecx, 1
+        mov [contadorFilas], ecx
+
+        jmp empieza
+    
+    mostrarMatriz:
+        call inicializarColumnas
+        call inicializarFilas
+
+        empiezaMuestra:
+            mov ecx, [contadorFilas] ; if ( contadorFilas > cantidadFilasYColumnas )
+            mov eax, [cantidadFilasYColumnas]
+            sub eax, 1
+            cmp ecx, eax
+            jg salirDelPrograma
+            
+            call mostrarParteMatriz
+            call mostrarEspacio
+
+            mov ecx, [contadorColumnas] ; contadorColumnas++
+            add ecx, 1
+            mov [contadorColumnas], ecx
+
+            mov eax, [cantidadFilasYColumnas]
+            sub eax, 1
+            cmp ecx, eax ; if ( contadorColumnas <= cantidadFilasYColumnas )
+            jle empiezaMuestra
+
+            call inicializarColumnas ; contadorColumnas = 0
+
+            call mostrarSaltoDeLinea
+
+            mov ecx, [contadorFilas] ; contadorColumnas++
+            add ecx, 1
+            mov [contadorFilas], ecx
+
+            jmp empiezaMuestra
